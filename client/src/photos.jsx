a586fb6468898916bc7cc43bbe2e78e56c9fc579
Steve Hallam <StevenAHallam@gmail.com>
2018-03-01 09:27:24 -0800

    Implemented CSS Modules and replaced footer count with flag icon.

--- client/src/photos.jsx
+++ client/src/photos.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 import axios from 'axios';
+import styles from './photos.css';
 
 class App extends React.Component {
   constructor(props) {
@@ -63,7 +64,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <div className="test">
+        <div className={styles.app}>
         <Gallery photos={this.state.photos} 
           onClick={this.openLightbox} 
           columns={Math.ceil(this.state.photos.length/2)} //Makes enough columns so there will be always be two rows
@@ -74,7 +75,19 @@ class App extends React.Component {
           onClickNext={this.gotoNext}
           currentImage={this.state.currentImage}
           isOpen={this.state.lightboxIsOpen}
-          showImageCount={false}
+          showImageCount={true}
+          theme={{ //Use this to modify the css stlyings of the lightbox *remember camelcase!*
+            footer: {
+              fontFamily: 'Arial'
+            },
+            footerCount:{ //Replaces footer count with a flag icon
+              backgroundImage: 'url("http://res.cloudinary.com/hallam/image/upload/v1519924638/flag.png")',
+              backgroundSize: 'cover',
+              width:'20px',
+              height:'20px',
+              fontSize: '0'
+            }
+          }}
         />
         </div>
       </div>
